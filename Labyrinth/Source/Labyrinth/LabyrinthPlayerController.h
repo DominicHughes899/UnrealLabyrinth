86fbf4085239e2836570af96be521c136857b7ac
DominicHughes899 <91923512+DominicHughes899@users.noreply.github.com>
2024-02-18 11:08:37 +0000

    feat(env): added low poly asset pack

---
+++ Labyrinth/Source/Labyrinth/LabyrinthPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LabyrinthPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LABYRINTH_API ALabyrinthPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
